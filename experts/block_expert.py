2ef0f760d57727b9a0c8a6404bae9d0b867a534c
KevinHTyrrell <KevinHTyrrell@gmail.com>
2020-11-18 19:37:20 -0500

    Removed unnecessary init

---
+++ experts/block_expert.py
@@ -0,0 +1,29 @@
+from experts.base_expert import BaseExpert
+
+
+class BlockExpert(BaseExpert):
+    def _config(self):
+        param_dict = {
+            'activation'                : None,
+            'batch_norm'                : 'transfer',
+            'dilution'                  : 'transfer',
+            'dropout'                   : 'transfer',
+            'filters'                   : 'train',
+            'flatten_input'             : 'transform',
+            'flatten_output'            : 'transfer',
+            'kernel_initializer'        : 'train',
+            'kernel_regularizer'        : 'train',
+            'kernel_size'               : 'train',
+            'max_pool'                  : 'transfer',
+            'padding'                   : 'train',
+            'reshape'                   : 'transfer',
+            'strides'                   : 'train',
+            'units'                     : 'train',
+            'upsample_input'            : 'transform',
+            'upsample_output'           : 'transfer',
+            'type'                      : 'train'
+        }
+        self._args.update({'parameters': param_dict})
+    def get_param_block(self, param_name: str):
+        return self._args.get('parameters').get(param_name)
+
