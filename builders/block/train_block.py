80dabd4223a490e0889eaa20f4c1d75da9dca3d4
KevinHTyrrell <KevinHTyrrell@gmail.com>
2020-11-19 19:45:44 -0500

    Parse hyperparameters and build main layer

--- builders/block/train_block.py
+++ builders/block/train_block.py
@@ -1,8 +1,18 @@
-from builders.block.base_block import BaseBlock, keras
+from tensorflow import keras
+from mappers.arg_mapper import ArgMapper
+from builders.block.base_block import BaseBlock
 
 
 class TrainBlock(BaseBlock):
     def build(self, input_layer: keras.layers.Layer):
-        # take type out of hyperparameters and call that layer #
-        # returns last layer to feed into next block #
-        pass
+        param_dict = dict()
+        hyperparameters = self._args.get('hyperparameters')
+        layer_type = hyperparameters['type'].get('val')
+        del hyperparameters['type']
+        layer_skeleton = self._layer_expert.get_layer(layer_type)
+        for arg_name, arg_val in hyperparameters.items():
+            arg_val_built = self._arg_mapper.map_arg(arg_name=arg_name, arg_val=arg_val)
+            param_dict.update(arg_val_built)
+        layer_built = layer_skeleton(**param_dict)
+        self._args['layer'] = layer_built
+        self._args['output_layer'] = layer_built
