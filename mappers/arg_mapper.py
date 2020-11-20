591b9c1d7476a83ecce8127443eaa43619833a56
KevinHTyrrell <KevinHTyrrell@gmail.com>
2020-11-19 19:29:31 -0500

    Modified mappers

--- mappers/arg_mapper.py
+++ mappers/arg_mapper.py
@@ -1,3 +1,4 @@
+from tensorflow import keras
 from mappers.base_mapper import BaseMapper
 
 
@@ -10,18 +11,31 @@ class ArgMapper(BaseMapper):
         init_dict = dict()
         for init_name, init_fn in keras.initializers.__dict__.items():
             if isinstance(init_fn, type):
-                init_dict.update({init_name: init_fn})
+                init_dict.update({init_name.lower(): init_fn})
         self._args['initializers'] = init_dict
 
     def _load_regularizers(self):
         reg_dict = dict()
         for init_name, init_fn in keras.regularizers.__dict__.items():
             if isinstance(init_fn, type):
-                reg_dict.update({init_name: init_fn})
+                reg_dict.update({init_name.lower(): init_fn})
         self._args['regularizers'] = reg_dict
 
-    def get_initializer(self, init_name):
+    def _get_initializer(self, init_name):
         return self._args['initializers'].get(init_name.lower())
 
-    def get_regularizer(self, reg_name):
+    def _get_regularizer(self, reg_name):
         return self._args['regularizers'].get(reg_name.lower())
+
+    def map_arg(self, arg_name: str, arg_val: dict):
+        if not arg_val.get('complex'):
+            return {arg_name.lower(): arg_val.get('val')}
+        if arg_name.find('initializer') != -1:
+            arg_fn = self._get_initializer(arg_val.get('name'))
+            arg_fn_built = arg_fn(*arg_val.get('val'))
+            return {arg_name.lower(): arg_fn_built}
+        if arg_name.find('regularizer') != -1:
+            arg_fn = self._get_regularizer(arg_val.get('name'))
+            arg_fn_built = arg_fn(*arg_val.get('val'))
+            return {arg_name.lower(): arg_fn_built}
+        return {arg_name, None}
--- mappers/base_mapper.py
+++ mappers/base_mapper.py
@@ -4,3 +4,7 @@ from abc import ABC
 class BaseMapper(ABC):
     def __init__(self):
         self._args = dict()
+        self._config()
+
+    def _config(self):
+        raise NotImplementedError
